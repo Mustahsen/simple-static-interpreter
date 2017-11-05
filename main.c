549b287c4e3ef3fb334eeb1c34308e411d6d5eea
Erham Mergen <EMergen1@rockwellautomation.com>
2017-11-06 01:49:08 +0300

    Initial code commit

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+#define	PCHAR	82
+#define PLIMIT	1024
+
+int main(){
+	
+	double p_storage[PLIMIT];
+	
+	for(int i = 0; i < PLIMIT; ++i){
+		p_storage[i] = 0.0;
+	}
+	
+	
+	
+	
+	return 0;
+}
